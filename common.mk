3939a555a4621ed4a0f02644cd5e1f228431979a
juliohm1978 <jhm@juliohm.com.br>
2020-05-07 00:33:40 -0300

    fix chart appVersion to 1.0.0

--- common.mk
+++ common.mk
@@ -1,5 +1,5 @@
 PYENV_VERSION=3.6.10
-CONTROLLER_TAG=juliohm/kubeless-pulsar-trigger-controller:1.0
-DISPATCHER_TAG=juliohm/kubeless-pulsar-trigger-dispatcher:1.0
+CONTROLLER_TAG=juliohm/kubeless-pulsar-trigger-controller:1.0.0
+DISPATCHER_TAG=juliohm/kubeless-pulsar-trigger-dispatcher:1.0.0
 
 .PHONY: $(MAKECMDGOALS)
